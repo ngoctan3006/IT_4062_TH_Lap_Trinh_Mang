973f968a62eb97a4d3453d8302a6835bd641c7b0
ngoctan3006 <ngoctan1k2014@gmail.com>
2021-10-20 11:51:02 +0700

    add assignment 4

---
+++ assignment4/client/main.c
@@ -0,0 +1,65 @@
+/*UDP Echo Client*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFF_SIZE 1024
+
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("Usage: ./client IPAddress PortNumber\n");
+        return 1;
+    }
+    int client_sock;
+    char buff[BUFF_SIZE];
+    struct sockaddr_in server_addr;
+    int bytes_sent, bytes_received;
+    socklen_t sin_size;
+    
+    //Step 1: Construct a UDP socket
+    if ((client_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        /* calls socket() */
+        perror("\nError: ");
+        exit(2);
+    }
+
+    //Step 2: Define the address of the server
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[2]));
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    while(1) {
+        //Step 3: Communicate with server
+        printf("\nInsert string to send (Ctrl + C to quit): ");
+        memset(buff,'\0',(strlen(buff) + 1));
+        scanf("%[^\n]s", buff);
+        while(getchar() != '\n');
+        
+        sin_size = (socklen_t) sizeof(struct sockaddr);
+        
+        bytes_sent = sendto(client_sock, buff, strlen(buff), 0, (struct sockaddr *) &server_addr, sin_size);
+        if (bytes_sent < 0) {
+            perror("Error: ");
+            close(client_sock);
+            return 2;
+        }
+
+        bytes_received = recvfrom(client_sock, buff, BUFF_SIZE - 1, 0, (struct sockaddr *) &server_addr, &sin_size);
+        if (bytes_received < 0) {
+            perror("Error: ");
+            close(client_sock);
+            return 2;
+        }
+        buff[bytes_received] = '\0';
+        printf("Reply from server:\n%s\n", buff);
+    }
+        
+    close(client_sock);
+    return 0;
+}
---
+++ assignment4/server/main.c
@@ -0,0 +1,97 @@
+/*UDP Echo Server*/
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define BUFF_SIZE 1024
+
+char *handleString(char *string) {
+    if (strlen(string) == 0) {
+        return NULL;
+    }
+    int countNumber = 0, countLetter = 0;
+    char *res = malloc(BUFF_SIZE + 1);
+    char number[BUFF_SIZE], letter[BUFF_SIZE];
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (isdigit(string[i])) {
+            number[countNumber++] = string[i];
+        } else if (isalpha(string[i])) {
+            letter[countLetter++] = string[i];
+        } else {
+            return NULL;
+        }
+    }
+    number[countNumber] = '\0';
+    letter[countLetter] = '\0';
+    sprintf(res, "%s\n%s", number, letter);
+    return res;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Not found port number\n");
+        return 1;
+    }
+
+    int server_sock; /* file descriptors */
+    char buff[BUFF_SIZE];
+    int bytes_sent, bytes_received;
+    struct sockaddr_in server; /* server's address information */
+    struct sockaddr_in client; /* client's address information */
+    socklen_t sin_size;
+
+    //Step 1: Construct a UDP socket
+    if ((server_sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        /* calls socket() */
+        perror("\nError: ");
+        exit(2);
+    }
+
+    //Step 2: Bind address to socket
+    server.sin_family = AF_INET;
+    server.sin_port = htons(atoi(argv[1])); /* Remember htons() from "Conversions" section? =) */
+    server.sin_addr.s_addr = INADDR_ANY; /* INADDR_ANY puts your IP address automatically */
+    bzero(&(server.sin_zero), 8); /* zero the rest of the structure */
+
+    if (bind(server_sock, (struct sockaddr *)&server, (socklen_t) sizeof(struct sockaddr)) == -1) {
+        /* calls bind() */
+        perror("\nError: ");
+        exit(2);
+    }
+
+    //Step 3: Communicate with clients
+    while (1) {
+        sin_size = sizeof(struct sockaddr_in);
+
+        bytes_received = recvfrom(server_sock, buff, BUFF_SIZE - 1, 0, (struct sockaddr *)&client, &sin_size);
+
+        if (bytes_received < 0)
+            perror("\nError: ");
+        else {
+            // Handle string
+            buff[bytes_received] = '\0';
+            char *res = handleString(buff);
+            if (res == NULL) {
+                strcpy(res, "Error");
+            }
+            bytes_sent = sendto(server_sock, res, strlen(res), 0, (struct sockaddr *) &client, sin_size);
+            if (bytes_sent < 0) {
+                perror("\nError: ");
+            }
+        }
+
+        bytes_sent = sendto(server_sock, buff, bytes_received, 0, (struct sockaddr *)&client, sin_size); /* send to the client welcome message */
+        if (bytes_sent < 0) {
+            perror("\nError: ");
+        }
+    }
+
+    close(server_sock);
+    return 0;
+}
